fd929c6d1fd5934526bfd6e3a3ea1a1948997970
Karim Mohamed <komicho1996@gmail.com>
2025-11-03 01:03:48 +0200

    Fix: correct return variable in infolist method of ViewAttribute class

--- src/Filament/Resources/AttributeResource/Pages/ViewAttribute.php
+++ src/Filament/Resources/AttributeResource/Pages/ViewAttribute.php
@@ -20,7 +20,7 @@ class ViewAttribute extends ViewRecord
 
     public function infolist(Schema $schema): Schema
     {
-        return $infolist
+        return $schema
             ->schema([
                 Tabs::make('Tabs')->tabs([
                     Tab::make('Order Information')
